3bdf488237371dc1b27cf0a36364342c7d8db8d1
HannibalMontannibal <33308759+HannibalMontannibal@users.noreply.github.com>
2017-12-17 20:14:17 +1100

    Pause Menu
    Pause Menu quits the game when you press Quit, and continues the game when you press Continue.
    Additionally, when the Pause Menu is up the cursor is visibly, but otherwise it is invisible.

--- Roguelite/Assets/Scripts/PauseMenu.cs
+++ Roguelite/Assets/Scripts/PauseMenu.cs
@@ -15,18 +15,35 @@ public class PauseMenu : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{
+		//pauses the game and brings up the menu when escape has been hit.
+		//also unpauses the menu when 'Escape' is hit a second time.
 		if (isPaused) {
 			pauseMenu.SetActive (true);
 			Time.timeScale = 0f;
+			Cursor.visible = true;
 		} else 
 		{
 			pauseMenu.SetActive (false);
 			Time.timeScale = 1f;
+			Cursor.visible = false;
+
 		}
 
 		if (Input.GetKeyDown (KeyCode.Escape)) 
 		{
 			isPaused = !isPaused;
 		}
+	}
+
+	public void QuitGame()
+	{
+		//Quits the game when Built
+		//Debug.Log ("Game has been quitted");
+		Application.Quit ();
+	}
+
+	public void Continue()
+	{
+		isPaused = false;
 	}
 }
