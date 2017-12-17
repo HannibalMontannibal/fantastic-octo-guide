69871120b93b03836d9d4b70c337f28e5b8325ce
HannibalMontannibal <33308759+HannibalMontannibal@users.noreply.github.com>
2017-12-17 14:29:02 +1100

    Main Menu works!
    Made a working main menu that loads the game when clicked.

---
+++ Roguelite/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	//plays the game when you press the button.
+	public void ToGame()
+	{
+		SceneManager.LoadScene ("MainGame");
+	}
+}
